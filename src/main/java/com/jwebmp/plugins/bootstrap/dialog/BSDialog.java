f5e87658a3cd4db529f3cdd81c12b41ef76ba490
GedMarc <ged_marc@hotmail.com>
2019-01-17 22:00:32 +0200

    GPG Update

--- src/main/java/com/jwebmp/plugins/bootstrap/dialog/BSDialog.java
+++ src/main/java/com/jwebmp/plugins/bootstrap/dialog/BSDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 Marc Magon
+ * Copyright (C) 2017 GedMarc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ import java.util.Objects;
  *
  * @param <J>
  *
- * @author Marc Magon
+ * @author GedMarc
  * @version 1.0
  * @since 29 Aug 2015
  */
