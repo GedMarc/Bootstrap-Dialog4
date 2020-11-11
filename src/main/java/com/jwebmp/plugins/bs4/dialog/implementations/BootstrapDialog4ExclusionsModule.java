b49a3adcfd7ac90f7d760a14be7d788c9728f3fa
GedMarc <ged_marc@hotmail.com>
2020-11-12 01:18:51 +0200

    Jakarta Namespace - JLink building

--- src/main/java/com/jwebmp/plugins/bs4/dialog/implementations/BootstrapDialog4ExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/bs4/dialog/implementations/BootstrapDialog4ExclusionsModule.java
@@ -2,7 +2,7 @@ package com.jwebmp.plugins.bs4.dialog.implementations;
 
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
